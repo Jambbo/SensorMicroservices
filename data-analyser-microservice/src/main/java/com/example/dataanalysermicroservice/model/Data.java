8f50ef8182229443cc6c0104368e6f647f08929e
Hlib Chibichian <chibgleb99@gmail.com>
2024-05-31 11:22:54 +0100

    added saving to db and docker configurations

--- data-analyser-microservice/src/main/java/com/example/dataanalysermicroservice/model/Data.java
+++ data-analyser-microservice/src/main/java/com/example/dataanalysermicroservice/model/Data.java
@@ -1,5 +1,6 @@
 package com.example.dataanalysermicroservice.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,15 +8,23 @@ import lombok.ToString;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "data")
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
 public class Data {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private Long sensorId;
     private LocalDateTime timestamp; //time when the sensor sent an info
     private double measurement; // measurement that the sensor sent
+
+    @Column(name = "type")
+    @Enumerated(value = EnumType.STRING)
     private MeasurementType measurementType;
 
     public enum MeasurementType {
